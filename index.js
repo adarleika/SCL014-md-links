71c4f669de51673cad72d724ae8506193968e614
adarleika <adarleika@gmail.com>
2020-11-04 09:35:02 -0300

    create package.json

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
 };
+*/
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1> hola mundo </h1>");
+});
+
+app.listen(3000, () => console.log("Escuchando el puerto 3000 ..."));
